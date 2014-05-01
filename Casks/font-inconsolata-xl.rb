faf9ca8be7724722df337dffac49d0547935ae2d
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2014-04-30 17:30:20 -0700

    Added Inconsolata XL cask

---
+++ Casks/font-inconsolata-xl.rb
@@ -0,0 +1,8 @@
+class FontInconsolataXl < Cask
+  url 'http://www.bitcetera.com/page_attachments/0000/0023/InconsolataXL.zip'
+  homepage 'http://www.bitcetera.com/en/techblog/2009/10/09/inconsolata-xl-font/'
+  version '001.010'
+  sha256 'b4626a01bca0a6b554c07d9d67d85a1ebd1bf62b462bbd5f38732f49eb34d253'
+  font 'InconsolataXL/Inconsolata XL.otf'
+  font 'InconsolataXL/Inconsolata XL Bold.otf'
+end
