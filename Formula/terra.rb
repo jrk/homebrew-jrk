6dc33de93b8a0acb6b753972915382fa44efac07
Jonathan Ragan-Kelley <jrk@cs.stanford.edu>
2015-10-30 17:07:40 -0700

    Update Terra to 2015-08

--- Formula/terra.rb
+++ Formula/terra.rb
@@ -1,23 +1,22 @@
 class Terra < Formula
   desc "Terra language"
   homepage "http://terralang.org"
-  url "https://github.com/zdevito/terra/releases/download/release-2015-03-12/terra-OSX-x86_64-04fb3e2.zip"
-  version "2015-03-12"
-  sha256 "475e046e52ba4d2582d059acadb2ce876c47593aafa3e1311fb6ba5ae1827c99"
+  url "https://github.com/zdevito/terra/releases/download/release-2015-08-03/terra-OSX-x86_64-84bbb0b.zip"
+  version "2015-08-03"
+  sha256 "53b2473629c8ec03046d189b7ebfa13b12c2dabc5f0b2731fe7e4cc4abf7c82d"
 
   def install
-    lib.install 'libterra.so'
-    bin.install 'terra'
-    system 'mkdir', 'inc'
-    system 'mv', 'include', 'inc/terra'
-    include.install 'inc/terra'
-    system 'mkdir', '-p', 'share/terra'
-    system 'mv', 'tests', 'share/terra/tests'
+    lib.install 'lib/libterra.a'
+    lib.install 'lib/libterra_dynamic.so'
+    bin.install 'bin/terra'
+    include.install 'include'
     share.install 'share/terra'
   end
 
   test do
-    #system "#{bin}/terra", "#{share}/terra/tests/run" # doesn't work yet
-    system "false"
+    Dir.chdir("#{share}/terra/tests") do
+      system "#{bin}/terra", "run"
+    end
+    #system "false"
   end
 end
